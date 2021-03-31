be595d66c114f6993d812856d79aedc2789502f7
rafaelciebra <rafael.ciebra@bandtec.com.br>
2021-03-31 10:35:20 -0300

    1 commit

---
+++ treinameto-kotlin/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "treinamento-kotlin"
+
---
+++ treinameto-kotlin/src/main/kotlin/TiposVariaveis.kt
@@ -0,0 +1,8 @@
+class TiposVariaveis {
+
+    fun main (args: Array<String>){
+
+        var numero = 12345
+        println(numero)
+    }
+}
